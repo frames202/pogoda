798f0719a95ddf07b870f50ad163f0d8f493a3bd
ex <ex3819@gmail.com>
2021-03-27 11:22:55 +0200

    nm

--- ex.py
+++ ex.py
@@ -5,7 +5,7 @@ init(convert = True)
 
 while a != '0':
 	a = input('chose a color:\nRed-1\nBlue-2\nGreen-3\nWhite-4\nPurple-5\nBlack-6\nYellow-7\nprint 0 to end: \n')
-	text = input('Input text here: ')
+	text = input('git mergeInput text here: ')
 	if a == '1':
 		print(Fore.RED + Style.BRIGHT + text + '\n' + Style.RESET_ALL)		
 
