32b2c9b2cbdab3ae15c83b535574c78a514f72dd
ex <ex3819@gmail.com>
2021-03-27 10:05:05 +0200

    'start'

---
+++ ex.py
@@ -0,0 +1,40 @@
+'''from colorama import init, Fore, Style,Back 					#colorama
+
+a = ''
+init(convert = True)
+
+while a != '0':
+	a = input('chose a color:\nRed-1\nBlue-2\nGreen-3\nWhite-4\nPurple-5\nBlack-6\nYellow-7\nprint 0 to end: \n')
+	text = input('Input text here: ')
+	if a == '1':
+		print(Fore.RED + Style.BRIGHT + text + '\n' + Style.RESET_ALL)		
+
+	elif a == '2':
+		print(Fore.BLUE + Style.BRIGHT + text + '\n' + Style.RESET_ALL)
+		
+
+	elif a == '3':
+		print(Fore.GREEN + Style.BRIGHT + text + '\n' + Style.RESET_ALL)
+		
+
+	elif a == '4':
+		print(Fore.WHITE + Style.BRIGHT + text + '\n' + Style.RESET_ALL)
+		
+
+	elif a == '5':
+		print(Fore.MAGENTA + Style.BRIGHT + text + '\n' + Style.RESET_ALL)
+		
+
+	elif a == '6':
+		print(Fore.BLACK + Back.WHITE + text + '\n' + Style.RESET_ALL)
+		
+
+	elif a == '7':
+		print(Fore.YELLOW + Style.BRIGHT + text + '\n' + Style.RESET_ALL)
+		
+'''
+
+'''from PIL import Image					#PIL
+f = Image.open("ex.png").show() 
+
+'''
---
+++ ex1.py
@@ -0,0 +1,28 @@
+import random as ran
+
+def ran_1000():
+	ans = []
+	for _ in range(0, 1000):
+		ans.append(ran.randint(0, 1000000))
+
+	return ans
+
+
+def filter(not_filtered_list):
+	filtered_list = []
+	for item in not_filtered_list:
+		if (item%10 + item%100//10) % 2 == 0:
+			filtered_list.append(item)
+
+	return filtered_list
+
+
+def smth(not_remaked):
+	remaked = []
+	for item in not_remaked:
+		remaked.append([item%10, item%100//10])
+
+	return remaked
+
+
+print(smth(filter(ran_1000())))
