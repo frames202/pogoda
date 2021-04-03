b19eb2f23f3272a121172ddea52d36d2f6690c7d
ex <ex3819@gmail.com>
2021-04-03 15:30:52 +0300

    longest

--- ex1.py
+++ ex1.py
@@ -30,7 +30,7 @@ print(smth(filter(ran_1000())))'''
 '''\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'''
 
 
-def gen(name_list, i = 0):
+'''def gen(name_list, i = 0):
 	while i != len(name_list)-1:
 		flag = yield name_list[i]
 		while flag == 'nextone':
@@ -49,3 +49,54 @@ print(func.send('nextone'))
 print(func.send('nextone'))
 print(func.__next__())
 print(func.send('nextone'))
+'''
+
+
+'''\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'''
+
+
+def big_pod(text):
+	used = []
+	longest = ''
+	for i in range(len(text)):
+		if text[i] not in used and text.count(text[i]) > 1:
+			if text[0] == text[i] and text[-1] == text[i]:
+				f = 2
+			elif text[0] == text[i] and text[-1] != text[i]:
+				f = 0
+			elif text[0] != text[i] and text[-1] == text[i]:
+				f = -1
+			elif text[0] != text[i] and text[-1] != text[i]:
+				f = None
+		else:
+			continue
+
+		if f == 2:
+			splited = text.split(text[i])
+			for j in splited:
+				longest = f'{text[i]}{j}{text[i]}' if len(j)+2 > len(longest) else longest
+				used.append(text[i])
+		elif f == 0:
+			splited = text.split(text[i])
+			splited.pop(-1)
+			for j in splited:
+				longest = f'{text[i]}{j}{text[i]}' if len(j)+2 > len(longest) else longest
+				used.append(text[i])
+		elif f == -1:
+			splited = text.split(text[i])
+			splited.pop(0)
+			for j in splited:
+				longest = f'{text[i]}{j}{text[i]}' if len(j)+2 > len(longest) else longest
+				used.append(text[i])
+		elif f == None:
+			splited = text.split(text[i])
+			splited.pop(0)
+			splited.pop(-1)
+			for j in splited:
+				longest = f'{text[i]}{j}{text[i]}' if len(j)+2 > len(longest) else longest
+				used.append(text[i])
+
+	return longest
+
+print(big_pod('sdkjfhlazxnmcbzmnxbcmznxbcmznxbcae'))
+print(big_pod('abcabeba'))
